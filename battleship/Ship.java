41084f0c334eb88bd4430beb8a60f4280f372510
Aman Bahata <bahataaman@gmail.com>
2016-12-26 16:07:49 +0000

    It adds accessor and mutator methods for the class

--- battleship/Ship.java
+++ battleship/Ship.java
@@ -10,5 +10,23 @@ public abstract class Ship {
     boolean horizontal;
     boolean [] hit;
 
+    public Ship(int length){
+        this.length = length;
+        this.hit = new boolean[length];
+    }
 
+    public int getBowRow(){
+        return this.bowRow;
+    }
+
+    public int getBowColumn(){
+        return this.bowColumn;
+    }
+
+    public boolean isHorizontal(){
+        return this.horizontal;
+    }
+    public int getLength(){
+        return this.length;
+    }
 }
