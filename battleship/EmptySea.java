9cb8a3912d7104a6a02d2aca1af4d8135345ac64
Aman Bahata <bahataaman@gmail.com>
2016-12-26 16:05:17 +0000

    It creates the required classes Battleship,Ship and subclassesof Ship - Cruiser, Destroyer, Battleship and EmptySea

---
+++ battleship/EmptySea.java
@@ -0,0 +1,7 @@
+package battleship;
+
+/**
+ * Created by aman1 on 26/12/2016.
+ */
+public class EmptySea extends Ship {
+}
---
+++ battleship/Ship.java
@@ -0,0 +1,8 @@
+package battleship;
+
+/**
+ * Created by aman1 on 26/12/2016.
+ */
+public abstract class Ship {
+
+}
