abc3dd2ee5aa48e31a0a977db99fc8c04c176e1d
Aman Bahata <bahataaman@gmail.com>
2016-12-26 16:06:25 +0000

    It adds instance variables

--- battleship/Ship.java
+++ battleship/Ship.java
@@ -4,5 +4,11 @@ package battleship;
  * Created by aman1 on 26/12/2016.
  */
 public abstract class Ship {
+    int bowRow;
+    int bowColumn;
+    int length;
+    boolean horizontal;
+    boolean [] hit;
+
 
 }
