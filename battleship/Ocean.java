87bc21cd65630f3c3b32c4516195f4f1dc3d4fe5
Aman Bahata <bahataaman@gmail.com>
2016-12-26 17:18:29 +0000

    It adds a constructor to class Ocean

--- battleship/Ocean.java
+++ battleship/Ocean.java
@@ -4,8 +4,28 @@ package battleship;
  * Created by aman1 on 26/12/2016.
  */
 public class Ocean {
-    Ship [][] ships;
-    int shotsFired;
-    int hitCount;
-    int shipsSunk;
+    private Ship [][] ships;
+    private int shotsFired;
+    private int hitCount;
+    private int shipsSunk;
+
+    public Ocean(){
+        ships = new Ship[10][10];
+        fillShips();
+        shotsFired = 0;
+        hitCount = 0;
+        shipsSunk = 0;
+    }
+
+    private void fillShips(){
+        for (int i = 0; i < ships.length; i++){
+            for (int j = 0; j<ships.length; j++){
+                ships[i][j] = new EmptySea();
+            }
+        }
+    }
+
+    public void placeAllShipsRandomly(){
+
+    }
 }
