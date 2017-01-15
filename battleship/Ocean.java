2bb21b11e694a09720fb34987124629094102329
Aman Bahata <bahataaman@gmail.com>
2017-01-15 14:40:11 +0000

    It adds instance variables to class Ocean

--- battleship/Ocean.java
+++ battleship/Ocean.java
@@ -4,4 +4,9 @@ package battleship;
  * Created by aman1 on 15/01/2017.
  */
 public class Ocean {
+    private Ship [][] ships;
+    private int shotsFired;
+    private int hitCount;
+    private int shipsSunk;
+
 }
