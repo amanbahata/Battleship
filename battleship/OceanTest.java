da0417e073389050c45672971e6be44a2a40890e
Aman Bahata <bahataaman@gmail.com>
2017-02-23 16:56:35 +0000

    Adds refactoring of methods in Ocean class and adds method print()

--- battleship/OceanTest.java
+++ battleship/OceanTest.java
@@ -1,55 +1,8 @@
-//package battleship;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-///**
-// * Created by aman1 on 15/01/2017.
-// */
-//class OceanTest {
-//
-//    Ocean ocean = new Ocean();
-//
-//
-//
-//    @Test
-//    public void testIsOccupied(){
-//        assertEquals(false, ocean.isOccupied(5,0));
-//    }
-//
-//    @Test
-//    public void testGetShipTypeAt(){
-//        String emptySea = "Empty sea";
-//        assertEquals(emptySea, ocean.getShipTypeAt(0,0));
-//    }
-//
-//    @Test
-//    public void testShootAt(){
-//        assertEquals(false, ocean.shootAt(0,3));
-//    }
-//
-//    @Test
-//    public void testPlaceAllShipsRandomly(){
-//        ocean.placeAllShipsRandomly();
-//        testGetShipArray();
-//    }
-//
-//    @Test
-//    public void testGetShipArray(){
-//        Ship[][] ships = ocean.getShipArray();
-//
-//        System.out.println();
-//
-//        for (int i=0; i< ships.length; i++){
-//            for (int j=0; j< ships[i].length; j++){
-//                System.out.print("   " + ships[i][j].getShipType());
-////                assertEquals("Empty sea", ships[i][j].getShipType() );
-//            }
-//            System.out.println();
-//        }
-//    }
-//
-//
-//
-//}
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by aman1 on 16/02/2017.
+ */
+class OceanTest {
+
+}
